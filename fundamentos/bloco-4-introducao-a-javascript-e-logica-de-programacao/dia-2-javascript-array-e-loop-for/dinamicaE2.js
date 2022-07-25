73ff10f477ad9b2ad9046e5821df1d0a76b5091c
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-07-25 19:16:20 -0300

    Exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/dinamicaE2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/dinamicaE2.js
@@ -1,13 +1,13 @@
-// 2. Crie um algoritmo que conte quantos números 
-// do intervalo entre 2 e 150 são divisíveis por 3. 
-// Caso a quantidade seja igual a 50, 
+// 2. Crie um algoritmo que conte quantos números
+// do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50,
 // exiba uma mensagem secreta.
 
 let num = 0;
 
 for (let i = 3; i <= 150; i += 3) {
-    num += 1;
+  num += 1;
 }
 if (num === 50) {
-    console.log("A mensagem secreta");
-} 
+  console.log("A mensagem secreta");
+}
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/dinamicaE3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/dinamicaE3.js
@@ -1,5 +1,5 @@
-// 3. Crie um algoritmo que recebe a idade de Nat, 
-// Renan e Sincero e imprime quem é a pessoa mais 
+// 3. Crie um algoritmo que recebe a idade de Nat,
+// Renan e Sincero e imprime quem é a pessoa mais
 // nova no formato: "Pessoa" é a mais nova.
 
 let Nat = 18;
@@ -7,9 +7,8 @@ let Renan = 19;
 let Sincero = 22;
 
 if (Nat < Renan && Nat < Sincero) {
-    console.log("Nat é a mais nova.");
 } else if (Renan < Sincero) {
-    console.log("Renan é o mais novo.");
+  console.log("Renan é o mais novo.");
 } else {
-console.log("Sincero é o mais novo.");
+  console.log("Sincero é o mais novo.");
 }
